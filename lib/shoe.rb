96323e2aed7bff39090acc8f037c40541da57733
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-24 18:32:53 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS.each do |brand_type|
-        BRANDS<< brand
-      
-     end
+      BRANDS.each do |brand_type|
+        if  brand != brand_type
+          BRANDS<< brand
+        end
+      end
   end
   
 
